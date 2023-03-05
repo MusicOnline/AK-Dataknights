9aef15feca459b45b00c53d035c09bed259f93d9
MusicOnline <39178127+MusicOnline@users.noreply.github.com>
2023-03-05 14:22:30 +0800

    feat(meta): add index page meta tags

--- app.vue
+++ app.vue
@@ -1,15 +1,56 @@
 <script setup lang="ts">
 import "~/assets/css/index.scss";
 
-const { locale } = useI18n();
+import { LocaleObject } from "@nuxtjs/i18n/dist/runtime/composables";
+import { useSeoMeta } from "@unhead/vue";
+import { ComputedRef } from "vue";
+
+const { t, locale, locales } = useI18n();
+
+useSeoMeta({
+  ogSiteName: () => t("general.siteIndexTitle"),
+  ogTitle: () => t("general.siteIndexTitleMeta"),
+  description: () => t("general.siteIndexDescription"),
+  ogDescription: () => t("general.siteIndexDescription"),
+  ogLocale: () => transformLocaleCode(locale.value),
+  ogImage: "/favicon-96x96.png",
+  ogImageType: "image/png",
+  ogImageWidth: "96",
+  ogImageHeight: "96",
+  twitterCard: "summary",
+});
+
+useHead({
+  title: () => t("general.siteIndexTitle"),
+  meta: () =>
+    (<ComputedRef<LocaleObject[]>>locales).value.flatMap(({ code }) => {
+      if (code === locale.value) return [];
+      return {
+        property: "og:locale:alternate",
+        content: transformLocaleCode(code),
+      };
+    }),
+});
+
+function transformLocaleCode(locale: string): string {
+  locale = locale.replace("-", "_");
+  switch (locale) {
+    case "en":
+      return "en_US";
+    case "ja":
+      return "ja_JP";
+    case "ko":
+      return "ko_KR";
+    case "zh":
+      return "zh_CN";
+  }
+  return locale;
+}
 </script>
 
 <template>
   <div>
     <Html :lang="locale"></Html>
-    <Head>
-      <Title>AK Dataknights</Title>
-    </Head>
     <NavigationBar />
     <main class="p-2">
       <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,87 @@ import { generateDataFiles } from "./tools/generate-data";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: "msapplication-TileColor", content: "#ffffff" },
+        { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+        { name: "theme-color", content: "#ffffff" },
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "57x57",
+          href: "/apple-icon-57x57.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "60x60",
+          href: "/apple-icon-60x60.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "72x72",
+          href: "/apple-icon-72x72.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "76x76",
+          href: "/apple-icon-76x76.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "114x114",
+          href: "/apple-icon-114x114.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "120x120",
+          href: "/apple-icon-120x120.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "144x144",
+          href: "/apple-icon-144x144.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "152x152",
+          href: "/apple-icon-152x152.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-icon-180x180.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/android-icon-192x192.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "96x96",
+          href: "/favicon-96x96.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/manifest.json" },
+      ],
+    },
+  },
   sourcemap: process.env.NODE_ENV !== "production",
   ssr: process.env.ENABLE_SSR?.toLowerCase() === "true",
   modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss", "nuxt-icon"],
