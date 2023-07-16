5d4185ea5da03fbaa52970ddb92db974bd20d0e0
MusicOnline <39178127+MusicOnline@users.noreply.github.com>
2023-07-16 10:07:10 +0800

    fix: bump nuxt-i18n to fix prefixed ssr

---
+++ i18n.config.ts
@@ -0,0 +1,25 @@
+import ENGLISH_DEFAULT from "./locales/en-US/default.json"
+import JAPANESE_DEFAULT from "./locales/ja-JP/default.json"
+import KOREAN_DEFAULT from "./locales/ko-KR/default.json"
+import CHINESE_DEFAULT from "./locales/zh-CN/default.json"
+
+export default defineI18nConfig(() => ({
+  fallbackWarn: false,
+  missingWarn: false,
+  legacy: false,
+  locale: "en",
+  fallbackLocale: {
+    // Issue: en falls back to en-US before en-TL
+    en: ["en-TL", "en-US", "zh-CN"],
+    zh: ["zh-CN"],
+    ja: ["ja-TL", "ja-JP", "zh-CN"],
+    ko: ["ko-TL", "ko-KR", "zh-CN"],
+    default: ["en-TL", "en-US", "zh-CN"],
+  },
+  messages: {
+    "en-US": ENGLISH_DEFAULT,
+    "ja-JP": JAPANESE_DEFAULT,
+    "ko-KR": KOREAN_DEFAULT,
+    "zh-CN": CHINESE_DEFAULT,
+  },
+}))
