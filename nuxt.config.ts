9bd8e89110e2b5f956674d92703e2d057b364de9
MusicOnline <39178127+MusicOnline@users.noreply.github.com>
2023-07-16 10:07:10 +0800

    fix: bump nuxt-i18n to fix prefixed ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,3 @@
-import ENGLISH_DEFAULT from "./locales/en-US/default.json"
-import JAPANESE_DEFAULT from "./locales/ja-JP/default.json"
-import KOREAN_DEFAULT from "./locales/ko-KR/default.json"
-import CHINESE_DEFAULT from "./locales/zh-CN/default.json"
 import { generateDataFiles } from "./tools/generate-data"
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
@@ -99,26 +95,7 @@ export default defineNuxtConfig({
     ],
     strategy: "prefix_and_default",
     defaultLocale: "en",
-    vueI18n: {
-      fallbackWarn: false,
-      missingWarn: false,
-      legacy: false,
-      locale: "en",
-      fallbackLocale: {
-        // Issue: en falls back to en-US before en-TL
-        en: ["en-TL", "en-US", "zh-CN"],
-        zh: ["zh-CN"],
-        ja: ["ja-TL", "ja-JP", "zh-CN"],
-        ko: ["ko-TL", "ko-KR", "zh-CN"],
-        default: ["en-TL", "en-US", "zh-CN"],
-      },
-      messages: {
-        "en-US": ENGLISH_DEFAULT,
-        "ja-JP": JAPANESE_DEFAULT,
-        "ko-KR": KOREAN_DEFAULT,
-        "zh-CN": CHINESE_DEFAULT,
-      },
-    },
+    compilation: { strictMessage: false },
   },
   hooks: {
     "build:before": async () => {
